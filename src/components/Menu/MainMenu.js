ce904156f96478df0b853816588c0ef8c72d5d2b
donniebrighto <damian.wawrzyniak97@gmail.com>
2019-11-01 23:58:25 +0100

    Removed active display on App logo

--- src/components/Menu/MainMenu.js
+++ src/components/Menu/MainMenu.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Container, Icon, Menu } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import MainNavigation from './MainNavigation';
 
 const MainMenu = () => {
   return (
     <Menu stackable>
       <Container>
-        <Menu.Item as={NavLink} to="/" exact header>
+        <Menu.Item as={Link} to="/" exact header>
           <Icon name="travel" />
           Kreator Podróży
         </Menu.Item>
